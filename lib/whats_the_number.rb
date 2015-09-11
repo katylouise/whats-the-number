5502021327faaf4c0b412b0cb97827df6f23dfb3
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-11 13:42:41 +0100

    Solved

--- lib/whats_the_number.rb
+++ lib/whats_the_number.rb
@@ -1,6 +1,13 @@
+require 'prime'
 class WhatsTheNumber
-  
+
   def self.sum_primes_less_than number
+    primes = []
+    Prime.each(number) do |prime|
+      primes << prime
+    end
+
+    primes.inject(:+)
   end
 
 end
